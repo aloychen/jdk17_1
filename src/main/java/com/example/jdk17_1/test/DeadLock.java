a9d2c2989249374a510767ef10c31091932cb631
248704126@qq.com <248704126@qq.com>
2022-01-19 18:29:01 +0800

    更新

--- src/main/java/com/example/jdk17_1/test/DeadLock.java
+++ src/main/java/com/example/jdk17_1/test/DeadLock.java
@@ -3,6 +3,10 @@ package com.example.jdk17_1.test;
 import com.example.jdk17_1.entity.Chopsticks;
 import com.example.jdk17_1.entity.Philosopher;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author
  * @date 2022/1/18
@@ -10,7 +14,43 @@ import com.example.jdk17_1.entity.Philosopher;
  */
 public class DeadLock {
     public static void main(String[] args) {
-        testDeadLock();
+//        testDeadLock();
+        testNum();
+    }
+
+    private static void testNum() {
+        long startTime = System.currentTimeMillis();
+        Long x =1000L;
+        BigInteger bigInteger = getBigInteger(x);
+        System.out.println(bigInteger);
+        long endTime = System.currentTimeMillis();
+        System.out.println("消耗时间："+(endTime-startTime));
+    }
+
+    private static BigInteger getBigInteger(Long x) {
+        Map<Long,BigInteger> bigIntegerMap = new HashMap<>();
+        return getBigIntegerByMap(x,bigIntegerMap);
+    }
+
+    private static BigInteger getBigIntegerByMap(Long x, Map<Long, BigInteger> bigIntegerMap) {
+        BigInteger bigInteger;
+        if(bigIntegerMap.containsKey(x)){
+            return bigIntegerMap.get(x);
+        }
+        if(x==0){
+            bigInteger = BigInteger.valueOf(0L);
+            bigIntegerMap.put(0L,bigInteger);
+            return bigInteger;
+        }
+
+        if(x==1||x==2){
+            bigInteger = BigInteger.valueOf(1);
+            bigIntegerMap.put(x,bigInteger);
+            return bigInteger;
+        }
+        BigInteger v = getBigIntegerByMap(x-1,bigIntegerMap).add(getBigIntegerByMap(x-2,bigIntegerMap));
+        bigIntegerMap.put(x,v);
+        return v;
     }
 
     private static void testDeadLock() {
